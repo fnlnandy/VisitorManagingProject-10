18f98436bd8b3c01fd12df0bc42a5274ce98a5d5
fnlnandy <andyfaniloniaina01@gmail.com>
2024-03-31 21:10:59 +0300

    Renamed default website

---
+++ src/components/MainPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="main-app">
+    <table class="list-table" id="visitors-list-table">
+      <tr>
+        <th>Nom</th>
+        <th>Nombre de jours</th>
+        <th>Tarif journalier</th>
+        <th>Tarif total (TJ * NJ)</th>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainPage',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
