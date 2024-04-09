d1c1d47f9a3dcaee085aef65d5c1960f81d3ae7b
fnlnandy <andyfaniloniaina01@gmail.com>
2024-04-09 20:06:35 +0300

    Documenting json.php

--- server/json.php
+++ server/json.php
@@ -2,22 +2,46 @@
 class JsonObject
 {
     private $mCore;
+    
+    /**
+     * Default constructor
+     */
     public function __construct()
     {
         $this->mCore = array();
     }
+
+    /**
+     * Sets a specific field of the JSON
+     * object
+     */
     public function setData(string $field, mixed $value): void
     {
         $this->mCore[$field] = $value;
     }
+
+    /**
+     * Sets a specific field as array in
+     * the JSON object
+     */
     public function setArray(string $field, array $array): void
     {
         $this->mCore[$field] = $array;
     }
+
+    /**
+     * Converts the core to JSON, to use as
+     * response
+     */
     public function toJson(): string | false
     {
         return json_encode($this->mCore);
     }
+
+    /**
+     * Wipes all the data inside the JSON
+     * object
+     */
     public function wipe(): void
     {
         $this->mCore = array();
