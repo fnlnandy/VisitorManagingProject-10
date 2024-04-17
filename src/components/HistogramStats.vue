5a17d6e50c512ae72b6f48f67acd36576589f71e
fnlnandy <andyfaniloniaina01@gmail.com>
2024-04-17 05:37:20 +0300

    Implemented a chart placeholder

--- src/components/HistogramStats.vue
+++ src/components/HistogramStats.vue
@@ -1,16 +1,33 @@
 <template>
-    <!-- TO-DO: Fill this up with actual graphical data -->
-    <p class="filler">{{ filler }}</p>
+    <canvas id="myChart"></canvas>
 </template>
 
-<script>
-export default {
-    name: 'HistogramStats',
-    props: {
-        filler: String,
+<script setup>
+    import Chart from 'chart.js/auto';
+    import { onMounted } from 'vue';
+
+    const labels = ['Total', 'Min', 'Max'];
+    const data = {
+        labels: labels,
+        datasets: [{
+            label: 'Foo',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: [40, 80, 12]
+        }]
+    };
+    const config = {
+        type: 'bar',
+        data: data,
+        options: {}
     }
-}
+
+    onMounted(() => {
+        new Chart(
+            document.getElementById("myChart"),
+            config
+        );
+    });
 </script>
 
-<style>
-</style>
+<style></style>
--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -8,76 +8,73 @@
     const loadedData = ref(false);
 
     onMounted(() => {
+        InitPage();
+    });
+
+    function InitPage() {
         let fetchReqParams = {
             init: true
         };
 
         SendFetchRequest("http://localhost/phpdir/exo10/server/fetchvisitors.php", fetchReqParams, true);
-    });
-
-    async function SendFetchRequest(dest, formData, fetchData = false)
-    {
-        console.log("Preparing fetch request to: ", dest, "With data: ", formData);
-      
-        fetch (dest, {method: 'POST', body: JSON.stringify(formData)})
-        .then(response => {
-            if (!response.ok) {
-                throw new Error("Network response wasn't ok.");
-            }
-
-            console.log("Getting the request response into JSON...");
-      
-            return response.json();
-        })
-        .then(data => {
-            if (fetchData)
-            {
-                fetchedData.value = data["FetchedData"];
-                loadedData.value  = true;
-            }
-            console.log("Fetched data assigned:", fetchedData.value);
-        })
-        .catch(error => {
-            console.error("There was a problem with the fetch operation: ", error);
-        });
     }
 
-    function ComputeFetchedData()
-    {
-        if (fetchedData.value.length > 0)
-        {
+    async function SendFetchRequest(dest, formData, fetchData = false) {
+        console.log("Preparing fetch request to: ", dest, "With data: ", formData);
+
+        fetch(dest, { method: 'POST', body: JSON.stringify(formData) })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error("Network response wasn't ok.");
+                }
+
+                console.log("Getting the request response into JSON...");
+
+                return response.json();
+            })
+            .then(data => {
+                if (fetchData) {
+                    fetchedData.value = data["FetchedData"];
+                    loadedData.value = true;
+                    console.log("Fetched data assigned:", fetchedData.value);
+                }
+            })
+            .catch(error => {
+                console.info("There was a problem with the fetch operation: ", error);
+            });
+    }
+
+    function ComputeFetchedData() {
+        if (fetchedData.value.length > 0) {
             console.log("Fetched data length is > 0");
             return fetchedData;
         }
-        else
-        {
+        else {
             console.log("Fetched data length == 0");
             return fetchedData;
         }
     }
 
-    function HandleActionOnVisitorEmitted(data)
-    {
-        SendFetchRequest("http://localhost/phpdir/exo10/server/actions.php", data);
+    async function HandleActionOnVisitorEmitted(data) {
+        await SendFetchRequest("http://localhost/phpdir/exo10/server/actions.php", data);
+        location.reload();
     }
 
-    function HandleDeletingVisitorEmitted(id)
-    {
+    async function HandleDeletingVisitorEmitted(id) {
         let data = { NumVisiteur: Number(id) };
 
-        SendFetchRequest("http://localhost/phpdir/exo10/server/rmvisitor.php", data);
+        await SendFetchRequest("http://localhost/phpdir/exo10/server/rmvisitor.php", data);
+        location.reload();
     }
 </script>
 
 <template>
-  <div class="main-app">
-    <VisitorsList v-if="loadedData" :visitorsArray="ComputeFetchedData()" @act-on-visitor-query="HandleActionOnVisitorEmitted" @delete-visitor-query="HandleDeletingVisitorEmitted"/>
-    <TextualStats v-if="loadedData" :fetchedData="ComputeFetchedData()"/>
-
-    <div class="stats-histogram-wrapper">
-      <HistogramStats filler="FILLER" />
+    <div class="main-app">
+        <VisitorsList v-if="loadedData" :visitorsArray="ComputeFetchedData()"
+            @act-on-visitor-query="HandleActionOnVisitorEmitted" @delete-visitor-query="HandleDeletingVisitorEmitted" />
+        <TextualStats v-if="loadedData" :fetchedData="ComputeFetchedData()" />
+        <HistogramStats />
     </div>
-  </div>
 </template>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -85,14 +82,17 @@
     h3 {
         margin: 40px 0 0;
     }
+
     ul {
         list-style-type: none;
         padding: 0;
     }
+
     li {
         display: inline-block;
         margin: 0 10px;
     }
+
     a {
         color: #42b983;
     }
